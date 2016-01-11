e91f67ceb5eb47a47cdb26291ab9e3680dc96940
Josh Hanson <joshhazn@gmail.com>
2016-01-10 21:15:39 -0500

    Initial Setup

---
+++ HPS.ASP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HPS.ASP.Startup))]
+namespace HPS.ASP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
