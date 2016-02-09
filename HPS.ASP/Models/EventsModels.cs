66e034f6fb21cc86c10595e3469a55691a6f2eff
Josh Hanson <joshhazn@gmail.com>
2016-02-08 23:57:50 -0500

    Added Events Model

---
+++ HPS.ASP/Models/EventsModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HPS.ASP.Models
+{
+  public class EventsModels
+  {
+    public int ID { get; set; }
+    public string EventName { get; set; }
+    public string EventDescription { get; set; }
+    public string EventLink { get; set; }
+    public DateTime EventDate { get; set; }
+    public byte EventFile { get; set; }
+  }
+}
