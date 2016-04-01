e0728b12e26643f742be33b73789bd1055023c25
Brandon Clusieau <clusieau@gmail.com>
2016-04-01 11:38:21 -0400

    user management start

---
+++ HPSMVC/Controllers/UserManagement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace HPSMVC.Controllers
+{
+    public class UserManagementController : Controller
+    {
+        public ActionResult Index()
+        {
+            var users = Membership.GetAllUsers();
+            return View(users);
+        }
+    }
+}
